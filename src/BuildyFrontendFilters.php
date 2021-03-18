1db726048e0d32fc8a18263409f04aebbefca637
Michael Rook <michael@handmadeweb.com.au>
2021-03-18 14:02:19 +1100

    Move chunk of dataAttrs into filter.

--- src/BuildyFrontendFilters.php
+++ src/BuildyFrontendFilters.php
@@ -18,7 +18,7 @@ class BuildyFrontendFilters
      * @var array
      */
     protected static $filters = [
-        //'handmadeweb-buildy_filter_all_data' => 'filter_all_data',
+        'handmadeweb-buildy_filter_all_data' => 'filter_all_data',
         'handmadeweb-buildy_filter_type:row' => 'filter_rows',
     ];
 
@@ -31,7 +31,80 @@ class BuildyFrontendFilters
 
     public static function filter_all_data($data)
     {
-        // Example Filter.
+        /**
+         * Generate the Bootstrap col-X-X classes for the current loop.
+         */
+        $columns = '';
+        if (! empty($data->options->columns)) {
+            foreach ($data->options->columns as $key => $val) {
+                if (! empty($val)) {
+                    // Legacy -- XS no longer exists and is defaulted to just col-val
+                    if ($key == 'xs') {
+                        // This is for backwards compatibility
+                        $columns .= "col-{$val} ";
+                    } else {
+                        $columns .= "col-{$key}-{$val} ";
+                    }
+                }
+            }
+            $columns = rtrim($columns, ' ');
+        }
+
+        /**
+         * Generate the spacing classes (margin/paddings) for each breakpoint size.
+         */
+        $margins = collect($data->inline->margin ?? []);
+        $paddings = collect($data->inline->padding ?? []);
+        $spacingClasses = '';
+
+        foreach ($margins as $breakpoint => $direction) {
+            $direction = collect($direction ?? []);
+            if (! $breakpoint || ! $direction) {
+                continue;
+            }
+            foreach ($direction as $name => $val) {
+                if (! $name || $val !== 0 && ! $val) {
+                    continue;
+                }
+                $first_char = substr($name, 0, 1);
+                $marginClasses = ($breakpoint === 'xs' ? '' : "$breakpoint:").'m'.$first_char.'-'.$val;
+                if (isset($spacingClasses)) {
+                    $spacingClasses .= " $marginClasses";
+                } else {
+                    $spacingClasses = $marginClasses;
+                }
+            }
+        }
+
+        foreach ($paddings as $breakpoint => $direction) {
+            $direction = collect($direction);
+
+            if (! $breakpoint || ! $direction) {
+                continue;
+            }
+
+            foreach ($direction as $name => $val) {
+                if (! $name || $val !== 0 && ! $val) {
+                    continue;
+                }
+                $first_char = substr($name, 0, 1);
+                $paddingClasses = ($breakpoint === 'xs' ? '' : "$breakpoint:").'p'.$first_char.'-'.$val;
+                if (isset($spacingClasses)) {
+                    $spacingClasses .= " $paddingClasses";
+                } else {
+                    $spacingClasses = $paddingClasses;
+                }
+            }
+        }
+
+        /*
+        * Append data to $data->generatedAttributes // $bladeData->generatedAttributes on the view.
+        */
+        $data->generatedAttributes = (object) [
+            'columns' => $columns,
+            'spacing' => $spacingClasses,
+        ];
+
         return $data;
     }
 
