2c4d6e1f35457adf89e47176a7d0aa4828ce1fcd
Michael Rook <michael@handmadeweb.com.au>
2021-03-22 18:24:37 +1100

    Minimize DB Queries to get post_content.

--- src/Traits/ContentCollector.php
+++ src/Traits/ContentCollector.php
@@ -6,6 +6,8 @@ use HandmadeWeb\Illuminate\Facades\DB;
 
 trait ContentCollector
 {
+    protected static $cache = [];
+
     /**
      * Undocumented function.
      *
@@ -15,16 +17,34 @@ trait ContentCollector
     public static function getContentForId($post_id)
     {
         if ($post_id !== 0) {
-            // if (! isset(static::$page_results[$post_id])) {
-            //     $post = DB::table('posts')->where('ID', $post_id)->first('post_content');
-            //     static::$page_results[$post_id] = $post;
-            // } else {
-            //     $post = static::$page_results[$post_id];
-            // }
-
-            $post = DB::table('posts')->where('ID', $post_id)->first('post_content');
+            $post = static::$cache[$post_id] ?? DB::table('posts')->where('ID', $post_id)->first('post_content');
 
             if (! empty($post->post_content)) {
+
+                // Cache of globals
+                $globals = collect(json_decode($post->post_content))->where('type', 'global-module');
+                if ($globals->count() > 0) {
+                    $globalsToQuery = [];
+                    foreach ($globals as $global) {
+                        if (! empty($global->content->id) && ! isset(static::$cache[$global->content->id])) {
+                            $globalsToQuery[] = $global->content->id;
+                        }
+                    }
+
+                    if (! empty($globalsToQuery)) {
+                        $globals = DB::table('posts')->where('post_type', 'bmcb-global')->whereIn('ID', $globalsToQuery)->get(['ID', 'post_content']);
+
+                        foreach ($globals as $global) {
+                            static::$cache[$global->ID] = $global;
+                        }
+                    }
+                }
+                // /Cache of globals
+
+                if (! isset(static::$cache[$post_id])) {
+                    static::$cache[$post_id] = $post;
+                }
+
                 return json_decode($post->post_content);
             }
         }
