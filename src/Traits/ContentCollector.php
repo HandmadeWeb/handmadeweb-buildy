9d4e89db22959fbb1689674326d0f413b165f8ad
Michael Rook <michael@handmadeweb.com.au>
2021-03-22 20:47:30 +1100

    Refactor Global prefetch.

--- src/Traits/ContentCollector.php
+++ src/Traits/ContentCollector.php
@@ -17,38 +17,52 @@ trait ContentCollector
     public static function getContentForId($post_id)
     {
         if ($post_id !== 0) {
-            $post = static::$cache[$post_id] ?? DB::table('posts')->where('ID', $post_id)->first('post_content');
+            if (! empty(static::$cache[$post_id])) {
+                $post = static::$cache[$post_id];
+            } else {
+                $post = DB::table('posts')->where('ID', $post_id)->first(['ID', 'post_content']);
+                $post->post_content = json_decode($post->post_content);
+            }
 
             if (! empty($post->post_content)) {
+                static::pushToCache($post);
+                static::preFetchGlobals($post);
 
-                // Cache of globals
-                $globals = collect(json_decode($post->post_content))->where('type', 'global-module');
-                if ($globals->count() > 0) {
-                    $globalsToQuery = [];
-                    foreach ($globals as $global) {
-                        if (! empty($global->content->id) && ! isset(static::$cache[$global->content->id])) {
-                            $globalsToQuery[] = $global->content->id;
-                        }
-                    }
-
-                    if (! empty($globalsToQuery)) {
-                        $globals = DB::table('posts')->where('post_type', 'bmcb-global')->whereIn('ID', $globalsToQuery)->get(['ID', 'post_content']);
-
-                        foreach ($globals as $global) {
-                            static::$cache[$global->ID] = $global;
-                        }
-                    }
-                }
-                // /Cache of globals
-
-                if (! isset(static::$cache[$post_id])) {
-                    static::$cache[$post_id] = $post;
-                }
-
-                return json_decode($post->post_content);
+                return static::$cache[$post->ID]->post_content;
             }
         }
 
         return [];
+    }
+
+    protected static function pushToCache($post)
+    {
+        if (! empty($post->ID) && ! isset(static::$cache[$post->ID])) {
+            static::$cache[$post->ID] = $post;
+        }
+    }
+
+    protected static function preFetchGlobals($post)
+    {
+        $globals = collect($post->post_content)->where('type', 'global-module');
+        if ($globals->count() > 0) {
+            $globalsToFetch = [];
+            foreach ($globals as $global) {
+                if (! empty($global->content->id) && ! isset(static::$cache[$global->content->id])) {
+                    $globalsToFetch[] = $global->content->id;
+                }
+            }
+
+            if (! empty($globalsToFetch)) {
+                $globals = DB::table('posts')->whereIn('ID', $globalsToFetch)->get(['ID', 'post_content']);
+
+                foreach ($globals as $global) {
+                    $global->post_content = json_decode($global->post_content);
+                    static::pushToCache($global);
+                }
+            }
+        }
+
+        return $globals->count();
     }
 }
