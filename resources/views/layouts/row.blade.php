088aeb42d7ee6350f0d7996f56f3d798a5822b6e
Michael Rook <michael@handmadeweb.com.au>
2021-05-11 15:42:25 +1000

    Row cleanup

--- resources/views/layouts/row.blade.php
+++ resources/views/layouts/row.blade.php
@@ -1,13 +1,9 @@
 @php
 
-$atts = $bladeData->attributes ?? null;
-
 $moduleClasses = '';
 $moduleID = '';
 
-$internalLinkEnabled = false;
-
-if (!empty($atts)) {
+if (!empty($bladeData->attributes)) {
   $moduleID = $bladeData->attributes->id ?? null;
   $moduleClasses = $bladeData->attributes->class ?? null;
   $internalLinkEnabled = $bladeData->attributes->in_page_link_enabled ?? false;
@@ -22,20 +18,8 @@ if (!empty($moduleStyle) && $moduleStyle !== 'none') {
   $moduleClasses .= " module-style__$moduleStyle";
 }
 
-$spacing = !empty($bladeData->generatedAttributes) ? $bladeData->generatedAttributes->spacing : null;
-$columns = collect($bladeData->content);
 
-$inline = $bladeData->inline ?? null;
-
-if (!empty($inline)) {
-  $textAlignxs = (string) ($bladeData->inline->textAlign->xs ?? null);
-  $textAlignlg = (string) ($bladeData->inline->textAlign->xl ?? null);
-
-  $bgSize = $bladeData->inline->backgroundImage->backgroundSize ?? "";
-  $bgPosition = $bladeData->inline->backgroundImage->backgroundPosition ?? "";
-  $bgRepeat = $bladeData->inline->backgroundImage->backgroundRepeat ?? null;
-  $bgColor = $bladeData->inline->backgroundColor ?? "";
-
+if (!empty($bladeData->inline)) {
   $bgImageSize = $bladeData->inline->backgroundImage->imageSize ?? "full";
   $bgImageURL = $bladeData->inline->backgroundImage->url ?? null;
   $bgImageID = $bladeData->inline->backgroundImage->imageID ?? null;
@@ -43,7 +27,6 @@ if (!empty($inline)) {
 
   // CSS GRID
   $enableCSSGrid = $bladeData->inline->cssGrid->enabled ?? null;
-  $cssGridGap = $bladeData->inline->cssGrid->gap ?? null;
 }
 
 
@@ -65,8 +48,8 @@ if (isset($internalLinkTarget)) {
 $dataAttString = null;
 
 // Add data atts to a string
-if (!empty($dataAtts)) {
-  foreach($dataAtts as $dataAtt) {
+if (!empty($bladeData->attributes->data) && count($bladeData->attributes->data)) {
+  foreach($bladeData->attributes->data as $dataAtt) {
     $name = strtolower($dataAtt->name);
     $value = stripslashes($dataAtt->value);
     $dataAttString .= " data-{$name}='{$value}' ";
@@ -81,7 +64,7 @@ if (!empty($enableCSSGrid) && $enableCSSGrid) {
     // This will become e.g grid-4-8
     $colClass = $gridPrefix;
 
-    foreach($columns as $column) {
+    foreach($bladeData->content as $column) {
         $colClass .= "-" . $column->options->columns->xl;
     }
 
@@ -89,26 +72,28 @@ if (!empty($enableCSSGrid) && $enableCSSGrid) {
 }
 
 // Temporary large/small version of text align, I'll loop this eventually
-if (!empty($textAlignxs)) {
-    !empty($moduleClasses) ? $moduleClasses .= " text-$textAlignxs" : $moduleClasses = "text-$textAlignxs";
+if (!empty($bladeData->inline->textAlign->xs)) {
+    !empty($moduleClasses) ? $moduleClasses .= " text-{$bladeData->inline->textAlign->xs}" : $moduleClasses = "text-{$bladeData->inline->textAlign->xs}";
 }
-if (!empty($textAlignlg)) {
-    !empty($moduleClasses) ? $moduleClasses .= " xl:text-$textAlignlg" : $moduleClasses = "xl:text-$textAlignlg";
+if (!empty($bladeData->inline->textAlign->xl)) {
+    !empty($moduleClasses) ? $moduleClasses .= " xl:text-{$bladeData->inline->textAlign->xl}" : $moduleClasses = "xl:text-{$bladeData->inline->textAlign->xl}";
 }
 /* Add responsive margin/padding classes if they're set */
-if (!empty($spacing)) {
-    !empty($moduleClasses) ? $moduleClasses .= " $spacing" : $moduleClasses = $spacing;
+if (!empty($bladeData->generatedAttributes->spacing)) {
+    !empty($moduleClasses) ? $moduleClasses .= " {$bladeData->generatedAttributes->spacing}" : $moduleClasses = $bladeData->generatedAttributes->spacing;
 }
 
 @endphp
 
 <div
     @isset($moduleID) id="{{ $moduleID }}" @endisset
-    @if($internalLinkEnabled)
+    @if($bladeData->attributes->in_page_link_enabled ?? false)
       data-internal_link_enabled="true" @endif
     @isset($internalLinkText) data-internal_link_text="{{ $internalLinkText }}" @endisset
     class="bmcb-row row {{ isset($moduleClasses) ? $moduleClasses : '' }}"
-    style="{{ $bladeData->generatedAttributes->inline_style ?? null }} {{ !empty($bladeData->inline->cssGrid->gap) ? "--col-gap: {$bladeData->inline->cssGrid->gap};" : null }} {{ !empty($bgImage) ? "background-image: url($bgImage);" : null }}"
+    style="{{ $bladeData->generatedAttributes->inline_style ?? null }} 
+    {{ !empty($bladeData->inline->cssGrid->gap) ? "--col-gap: {$bladeData->inline->cssGrid->gap};" : null }} 
+    {{ !empty($bgImage) ? "background-image: url($bgImage);" : null }}"
     @if(!empty($dataAttString))
       {!! $dataAttString !!}
     @endif>
