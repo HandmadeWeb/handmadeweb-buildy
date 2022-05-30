ff8ab334b400a5b306085de64488990563caebc3
Yohan <john@inertiainmotion.com.au>
2022-05-30 13:32:27 +1000

    Fix console warning with config being wrong type Force css extract in development mode

--- buildy-wp-gui/vue.config.js
+++ buildy-wp-gui/vue.config.js
@@ -1,17 +1,20 @@
 module.exports = {
-    chainWebpack: config => {
-      if(config.plugins.has('extract-css')) {
-        const extractCSSPlugin = config.plugin('extract-css')
-        extractCSSPlugin && extractCSSPlugin.tap(() => [{
-          filename: '[name].css',
-          chunkFilename: '[name].css'
-        }])
-      }
-    },
-    configureWebpack: {
-      output: {
-        filename: '[name].js',
-        chunkFilename: '[name].js'
-      }
+  chainWebpack: config => {
+    if (config.plugins.has('extract-css')) {
+      const extractCSSPlugin = config.plugin('extract-css')
+      extractCSSPlugin && extractCSSPlugin.tap(() => [{
+        filename: '[name].css',
+        chunkFilename: '[name].css'
+      }])
     }
+  },
+  configureWebpack: {
+    output: {
+      filename: '[name].js',
+      chunkFilename: '[name].js'
+    }
+  },
+  css: {
+    extract: true
   }
+}
