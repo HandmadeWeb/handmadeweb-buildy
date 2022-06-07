ea7561477d99ea5087e03720867a83bebe78d09a
Yohan <john@inertiainmotion.com.au>
2022-06-07 13:39:32 +1000

    Fix clone on row/section that does not contain ACF modules

--- buildy-wp-gui/src/components/modules_acf/acfHooks.js
+++ buildy-wp-gui/src/components/modules_acf/acfHooks.js
@@ -3,61 +3,61 @@ import { searchJSON } from '../../functions/jsonSearch'
 export const acfHooks = (vm) => {
   const cloneACFModule = async (resolve, reject, clone) => {
     const acfModules = searchJSON(clone, 'acf-module', 'type')
-    if (acfModules !== null && acfModules !== undefined && acfModules.length) {
-      // Loop over all acfModules that are found in any context (directly, row, section, column, etc)
-      const promises = acfModules.map(async (acfModule) => {
-        if (acfModule.content.acfForm.is_linked) {
-          vm.$vToast.warning(
-            'Cloning "linked" acf modules will not make a copy, instead it will clone them as a global, meaning they can overwrite each other.',
-            { duration: 6000 }
-          )
-          return
-        }
-        if (!acfModule?.content?.acfForm) {
-          return
-        }
+    if (acfModules === null || acfModules === undefined || !acfModules.length) return resolve(clone)
+    // Loop over all acfModules that are found in any context (directly, row, section, column, etc)
+    const promises = acfModules.map(async (acfModule) => {
+      if (acfModule.content.acfForm.is_linked) {
+        vm.$vToast.warning(
+          'Cloning "linked" acf modules will not make a copy, instead it will clone them as a global, meaning they can overwrite each other.',
+          { duration: 6000 }
+        )
+        return
+      }
+      if (!acfModule?.content?.acfForm) {
+        return
+      }
 
-        // Get the postID of the current one
-        const postID = acfModule.content.acfForm.post_id
+      // Get the postID of the current one
+      const postID = acfModule.content.acfForm.post_id
 
-        if (postID) {
-          const form = new FormData()
-          form.append('action', 'acf_duplicate_post')
-          form.append('post_id', postID)
-          form.append('nonce', window.global_vars.nonce)
+      if (postID) {
+        const form = new FormData()
+        form.append('action', 'acf_duplicate_post')
+        form.append('post_id', postID)
+        form.append('nonce', window.global_vars.nonce)
 
-          const params = new URLSearchParams(form)
-          const res = await fetch(window.global_vars.admin_ajax_url, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: params,
-          })
+        const params = new URLSearchParams(form)
+        const res = await fetch(window.global_vars.admin_ajax_url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          body: params,
+        })
 
-          const data = await res.json()
+        const data = await res.json()
 
-          if (data) {
-            acfModule.content.acfForm.post_id = data
-            if (acfModule.content.acfForm.hasOwnProperty('is_linked')) {
-              acfModule.content.acfForm.is_linked = false
-            }
-            acfModule.options.admin_label = `Custom Fields - ${acfModule.content.acfForm.field_groups_title} - ${data}`
-            return acfModule
+        if (data) {
+          acfModule.content.acfForm.post_id = data
+          if (acfModule.content.acfForm.hasOwnProperty('is_linked')) {
+            acfModule.content.acfForm.is_linked = false
           }
-          reject(new Error('Something went wrong with cloning'))
+          acfModule.options.admin_label = `Custom Fields - ${acfModule.content.acfForm.field_groups_title} - ${data}`
+          return acfModule
         }
+        reject(new Error('Something went wrong with cloning'))
+      }
 
-        // if postID isn't found, the clone will be bypassed and the module will clone as normal
-        acfModule.content.acfForm.post_id = null
-        acfModule.options.admin_label = 'Custom Fields'
-        return acfModule
-      })
+      // if postID isn't found, the clone will be bypassed and the module will clone as normal
+      acfModule.content.acfForm.post_id = null
+      acfModule.options.admin_label = 'Custom Fields'
+      return acfModule
+    })
 
-      await Promise.all(promises)
+    await Promise.all(promises)
 
-      resolve(clone)
-    }
+    resolve(clone)
+
   }
 
   const deleteACFModule = (resolve, reject, el) => {
